51589ef085fc13d362ce84dfa4037b084965f7da
matcool <26722564+matcool@users.noreply.github.com>
2021-01-26 21:51:17 -0300

    load all click sounds at once

--- clicks.py
+++ clicks.py
@@ -5,14 +5,17 @@ import random
 from pydub import AudioSegment
 import math
 
-def get_random_click_path(path: Path, type_: str):
-    if type_ == 'hold':
-        paths = glob.glob(str(path / 'holds' / '*.wav'))
-    elif type_ == 'soft':
-        paths = glob.glob(str(path / 'holds' / 's*.wav'))
-    elif type_ == 'release':
-        paths = glob.glob(str(path / 'releases' / '*.wav'))
-    return random.choice(paths)
+def load_clicks(base_path: Path):
+    holds = glob.glob(str(base_path / 'holds' / '*.wav'))
+    releases = glob.glob(str(base_path / 'releases' / '*.wav'))
+    hold_sounds = [AudioSegment.from_wav(i) for i in holds]
+    release_sounds = [AudioSegment.from_wav(i) for i in releases]
+    soft_sounds = [sound for sound, file_path in zip(hold_sounds, holds) if Path(file_path).name[0] == 's']
+    return {
+        'hold': hold_sounds,
+        'release': release_sounds,
+        'soft': soft_sounds
+    }
 
 def db2mag(db):
     return 10 ** (db / 20.0)
@@ -24,16 +27,15 @@ def nerve(x):
     return math.tanh(math.exp(0.1 * (x - 70.0) - 0.9)) / 2.0
 
 class Click:
-    def __init__(self, time: float, hold: bool, base_path: Path):
-        self.base_path = base_path
+    def __init__(self, time: float, hold: bool, click_sounds):
         self.time = time
         self.hold = hold
-        self.audio = AudioSegment.from_wav(get_random_click_path(base_path, 'hold' if hold else 'release'))
+        self.audio = random.choice(click_sounds['hold' if hold else 'release'])
 
     def mult_volume(self, amt: float):
         self.audio = self.audio.apply_gain(mag2db(db2mag(self.audio.dBFS) * amt) - self.audio.dBFS)
 
-    def process(self, last_click: 'Click', full_Length: float):
+    def process(self, last_click: 'Click', full_Length: float, click_sounds):
         bias = random.uniform(0.8, 1.2)
 
         if self.time > full_Length * 0.5:
@@ -46,7 +48,7 @@ class Click:
         time_since = self.time - last_click.time / 2 # why is it divided by two??
 
         if self.hold and time_since < 0.1:
-            self.audio = AudioSegment.from_wav(get_random_click_path(self.base_path, 'soft'))
+            self.audio = random.choice(click_sounds['soft'])
 
         if time_since > 0.1 or not self.hold:
             self.mult_volume(bias)
--- main.py
+++ main.py
@@ -30,7 +30,12 @@ replays.convert_to_time(replay, lvl)
 clicks_path = Path(args.clicks)
 
 print('Loading clicks')
-clicks = [clicks.Click(action.time, action.hold, clicks_path) for action in replay.actions]
+
+click_sounds = clicks.load_clicks(clicks_path)
+
+print('Generating clicks')
+
+clicks = [clicks.Click(action.time, action.hold, click_sounds) for action in replay.actions]
 full_length = clicks[-1].time + 1
 
 output = AudioSegment.silent(duration=full_length * 1000)
@@ -39,7 +44,7 @@ last_click = None
 last_click_hold = None
 for click in tqdm(clicks, desc='Mapping clicks'):
     if last_click is None or click.hold != last_click.hold:
-        click.process(last_click_hold, full_length)
+        click.process(last_click_hold, full_length, click_sounds)
         if click.hold: last_click_hold = click
         last_click = click
         output = output.overlay(click.audio, position=click.time * 1000)
